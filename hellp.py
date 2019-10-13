9820f60b14d4e9309d94f349c937e5e3770b7229
semikite <semikite@gmail.com>
2019-10-14 08:42:06 +0900

    private and hidden add

--- hellp.py
+++ hellp.py
@@ -9,21 +9,58 @@ class Dog:
     def wag(self):
         print("Dog's wag tail")
 
+    def m1(self):
+        print("m1")
+    
+    def m2():
+        print("m2")
+
     def __del__(self):
         print("desroy!!")
 
 class Puppy(Dog):
+    __hidden = "==========================="
     def __init__(self):
         self.name = "Puppy"
+        self.color = "red"
         print("Puppy was Born")
+
+    def __read_diary(self):
+        print("Diary Content!!!")
 
     def wag(self):
         print("Puppy's wag tail")
+        self.__read_diary()
+
+    def speak(self):
+        print("bow wow!", self.name)
+
+    def change_color(self):
+        self.color = "yellow"
+
+    def static_method():
+        print("xxxxxxxxxxxxxxxxxxxxxxx")
+
+    def hidden(self):
+        print(self.__hidden)
+
+d = Dog("dog")
+p = Puppy()
+
+d.speak()
+p.speak()
 
 
-puddle = Dog("puddle")
-puppy = Puppy()
+d.wag()
+p.wag()
 
-puddle.speak()  
-puppy.speak()
+print(d.name,p.name)
+print(p.color)
 
+p.change_color()
+
+print(p.color)
+
+p.hidden()
+
+Puppy.static_method()
